f0644305d67332999aacf1841d37f66e05c1f354
Jabari Holder <43169343+anddigital-jh@users.noreply.github.com>
2021-07-07 15:31:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import fs from "fs";
 export * from "./esm/high-level.js";
 import * as hdf5 from "./esm/high-level.js";
-
-const file = "./batch_0.fast5";
-var fileBuffer = fs.readFileSync(file, null).buffer;
-const convFile = new hdf5.File(fileBuffer, file);
-
-console.log(convFile);
 
 export default hdf5;
